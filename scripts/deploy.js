8c59dabebd79c56d287d1b94e27dc19a5e33684e
Peng Yang Guo <pyangguo@gmail.com>
2021-11-12 12:47:29 -0800

    Create a NFT with ERC1155

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,15 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-	const SuperMarioWorld = await ethers.getContractFactory('SuperMarioWorldOZ');
-	const superMarioWorld = await SuperMarioWorld.deploy('SuperMarioWorldOZ', 'SPRMO');
+	const SuperMarioWorld = await ethers.getContractFactory('SuperMarioWorldERC1155');
+	const superMarioWorld = await SuperMarioWorld.deploy('SuperMarioWorldERC1155', 'SPRME');
 
 	await superMarioWorld.deployed();
 	console.log('Success! Contract was deployed to: ', superMarioWorld.address);
 
 	await superMarioWorld.mint(
-		'https://ipfs.io/ipfs/Qmf5TwUGx4uCmmvzvwrckGRYg6aJXvshGiBV8Q5Txvsdcp'
+		10,
+		'https://ipfs.io/ipfs/QmQmvzMFFZcqhgcCB9QbzqMbYdMfXB4qSYVgWfqDrp2pHA'
 	);
 
 	console.log('NFT successfully minted');
